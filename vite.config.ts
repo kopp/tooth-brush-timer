c424b5259bae2b5e4e36feda5609f327b374795f
Michael Kopp <kopp.michael@yahoo.de>
2026-06-03 21:51:11 +0200

    allow to serve app from subdirectory

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // allow the app to be served from a subdirectory (as is custom on GitHub pages)
+  base: "./",
   plugins: [react()],
 })
